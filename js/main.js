14b9b3cc16b3d9d6780036f432972e06673edc1d
mizanavr <mizan@abrhub.com>
2022-09-14 17:10:50 +0600

    viewz  home page responsive

--- js/main.js
+++ js/main.js
@@ -1840,46 +1840,47 @@ $(document).ready(function () {
 });
 
 
-// slick slider for index page
-// $('.slider').slick({
-//     dots: false,
-//     infinite: true,
-//     speed: 300,
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     autoPlay: false,
-//     arrows: true,
-//     prevArrow: '<i class="fas fa-arrow-left prev"></i>',
-//     nextArrow: '<i class="fas fa-arrow-right next"></i>',
-//     responsive: [
-//         {
-//             breakpoint: 1024,
-//             settings: {
-//                 slidesToShow: 3,
-//                 slidesToScroll: 3,
-//                 infinite: true,
-//                 dots: true
-//             }
-//         },
-//         {
-//             breakpoint: 600,
-//             settings: {
-//                 slidesToShow: 2,
-//                 slidesToScroll: 2
-//             }
-//         },
-//         {
-//             breakpoint: 480,
-//             settings: {
-//                 slidesToShow: 1,
-//                 slidesToScroll: 1
-//             }
-//         }
-//         // You can unslick at a given breakpoint now by adding:
-//         // settings: "unslick"
-//         // instead of a settings object
-//     ]
-// });
+// slick slider blog page
+$('#recent-blog .items').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoPlay: true,
+    arrows: true,
+    centerPadding: '60px',
+    prevArrow: '<i class="fa-solid fa-circle-chevron-left prev"></i>',
+    nextArrow: '<i class="fa-solid fa-circle-chevron-right next"></i>',
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
 
 
 // /// variables////
