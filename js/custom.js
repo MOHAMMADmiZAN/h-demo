b7cbe8e73efb861aace1adf440b26b73be6f976d
mizanavr <mizan@abrhub.com>
2022-09-30 12:37:36 +0600

    new change

---
+++ js/custom.js
@@ -0,0 +1,188 @@
+// // slick slider blog page
+// $('#recent-blog .items').slick({
+//     dots: false,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 3,
+//     slidesToScroll: 3,
+//     autoPlay: true,
+//     arrows: true,
+//     centerPadding: '60px',
+//     prevArrow: '<i class="fa-solid fa-circle-chevron-left prev"></i>',
+//     nextArrow: '<i class="fa-solid fa-circle-chevron-right next"></i>',
+//     responsive: [
+//         {
+//             breakpoint: 1024,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 3,
+//                 infinite: true,
+//                 dots: true
+//             }
+//         },
+//         {
+//             breakpoint: 600,
+//             settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 2
+//             }
+//         },
+//         {
+//             breakpoint: 480,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1
+//             }
+//         }
+//         // You can unslick at a given breakpoint now by adding:
+//         // settings: "unslick"
+//         // instead of a settings object
+//     ]
+// });
+
+
+// /// variables////
+// const win = $(window);
+const html_body = $('html, body');
+// const stk = $('.st-menu');
+// const b2b = $('.back-top');
+// const c_stk = 'stiky';
+// const bdy = $('body');
+//
+//
+// //stiky-header//
+// win.scroll(() => win.scrollTop() > 300 ? stk.addClass(c_stk) : stk.removeClass(c_stk));
+//
+//
+// // animation scroll js
+$('.text a').on('click', function () {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+        let target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            html_body.animate({
+                scrollTop: target.offset().top - 0
+            }, 1500);
+            return false;
+        }
+
+    }
+});
+
+
+// accordion-start
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+    const itemToggle = this.getAttribute('aria-expanded');
+
+    for (let i = 0; i < items.length; i++) {
+        items[i].setAttribute('aria-expanded', 'false');
+    }
+
+    if (itemToggle === 'false') {
+        this.setAttribute('aria-expanded', 'true');
+    }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
+// accordion-end
+
+
+$('.navbar-collapse ul li a').click(() => $('.navbar-toggler:visible').click());
+let navbarCollapse = document.querySelector('.navbar-toggler');
+navbarCollapse.addEventListener('click', function (e) {
+
+    document.getElementById('navbarSupportedContent').classList.toggle('show');
+    document.querySelector('.show').style.bgColor = 'red';
+
+
+})
+let animatedText = document.querySelector('.animated-text');
+
+if (animatedText) {
+    const typed = new Typed('.animated-text', {
+        strings: ['No App', 'No coding', 'Compatible with any browser'],
+        typeSpeed: 60,
+        backSpeed: 60,
+        loop: true,
+        loopCount: Infinity,
+        startDelay: 100,
+
+    });
+}
+
+
+// legal page tab function
+
+let tabItems = document.querySelectorAll('.legalTabItems .tab');
+let tabContentItems = document.querySelectorAll('.legalTabContents .tab_content');
+
+const tabFilter = (id) => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    let tabContentId = `${id}_content`
+    for (const tabContentElement of tabContentItems) {
+        if (tabContentElement.id !== tabContentId) {
+            tabContentElement.classList.remove('tabActive');
+            if (id !== tabContentElement.id.split('_')[0]) {
+                document.getElementById(tabContentElement.id.split('_')[0]).classList.remove('tabItemActive')
+            }
+
+        } else {
+            document.getElementById(id).classList.add('tabItemActive');
+            tabContentElement.classList.add('tabActive');
+        }
+    }
+}
+
+const selectItem = (e) => {
+    const {id} = e.target;
+    tabFilter(id);
+}
+
+if (tabItems) {
+    for (const tabItem of tabItems) {
+        tabItem.addEventListener('click', selectItem);
+
+    }
+}
+
+// footer tab function
+const addTab = (e) => {
+    e.preventDefault()
+    window.location.href = `legal.html?tab=${e.target.id}`
+
+
+}
+(function () {
+    if (window.location.href.includes('tab')) {
+        const id = window.location.href.split('=')[1];
+        tabFilter(id);
+    }
+
+})()
+const footerTabItems = ['li a#terms', 'li a#privacy'];
+
+footerTabItems.forEach((item) => {
+    document.querySelector(item) && document.querySelector(item).addEventListener('click', addTab);
+})
+
+// control scroll to top legal page
+let scrollY = window.scrollY;
+window.addEventListener('scroll', () => {
+
+    let legalContent = document.querySelector('.legalTabContents')
+    if (legalContent) {
+        if (scrollY > (legalContent.clientHeight - 320)) {
+
+            document.querySelector('.legalTabItems').style.cssText = `position: relative;`
+
+
+        } else {
+            document.querySelector('.legalTabItems').style.cssText = `position: fixed; `
+        }
+    }
+
+
+})
